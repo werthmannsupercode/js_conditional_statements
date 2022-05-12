e51249d9ffefc2f0c09ae7466bda36a665e5a8a7
werthmannsupercode <werthmannsupercode@gmail.com>
2022-05-12 12:32:12 +0200

    bis Lev 2_7

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,3 +71,88 @@ function checkAirQuality() {
         bgColor.style.backgroundColor = "red";
     }
 }
+
+let bundeslandInfo = document.getElementById('bundeslandInfo');
+let bundeslandInfoErgebnis = document.getElementById('bundeslandInfoErgebnis');
+
+function check() {
+    // console.log(bundeslandInfo.value);
+    switch (bundeslandInfo.value) {
+        case "Bayern":
+            bundeslandInfoErgebnis.innerHTML = "Bayern hat 12,844 Mio Einwohner und München ist die Hauptstadt";
+            break;
+        case "Baden-Württemberg":
+        case "Badenwürttemberg":
+            bundeslandInfoErgebnis.innerHTML = "Baden-Württemberg hat 10,880 Mio Einwohner und Stuttgart ist die Hauptstadt";
+            break;
+        case "Berlin":
+            bundeslandInfoErgebnis.innerHTML = "Berlin hat 3,520 Mio Einwohner und Berlin ist die Hauptstadt";
+            break;
+        case "Brandenburg":
+            bundeslandInfoErgebnis.innerHTML = "Brandenburg hat 2,485 Mio Einwohner und Potsdam ist die Hauptstadt";
+            break;
+        case "Bremen":
+            bundeslandInfoErgebnis.innerHTML = "Bremen hat 0,671 Mio Einwohner und Bremen ist die Hauptstadt";
+            break;
+        case "Hamburg":
+            bundeslandInfoErgebnis.innerHTML = "Hamburg hat 1,787 Mio Einwohner und Hamburg ist die Hauptstadt";
+            break;
+        case "Hessen":
+            bundeslandInfoErgebnis.innerHTML = "Hessen hat 6,176 Mio Einwohner und Wiesbaden ist die Hauptstadt";
+            break;
+        case "Mecklenburg-Vorpommern":
+            bundeslandInfoErgebnis.innerHTML = "Mecklenburg-Vorpommern hat 1,612 Mio Einwohner und Schwerin ist die Hauptstadt";
+            break;
+        case "Niedersachsen":
+            bundeslandInfoErgebnis.innerHTML = "Niedersachsen hat 7,927 Mio Einwohner und Hannover ist die Hauptstadt";
+            break;
+        case "Nordrhein-Westfalen":
+            bundeslandInfoErgebnis.innerHTML = "Nordrhein-Westfalen hat 17,865 Mio Einwohner und Düsseldorf ist die Hauptstadt";
+            break;
+        case "Rheinland-Pfalz":
+            bundeslandInfoErgebnis.innerHTML = "Rheinland-Pfalz hat 4,053 Mio Einwohner und Mainz ist die Hauptstadt";
+            break;
+        case "Saarland":
+            bundeslandInfoErgebnis.innerHTML = "Saarland hat 0,996 Mio Einwohner und Saarbrücken ist die Hauptstadt";
+            break;
+        case "Sachsen":
+            bundeslandInfoErgebnis.innerHTML = "Sachsen hat 4,085 Mio Einwohner und Dresden ist die Hauptstadt";
+            break;
+        case "Sachsen-Anhalt":
+            bundeslandInfoErgebnis.innerHTML = "Sachsen-Anhalt hat 2,245 Mio Einwohner und Magdeburg ist die Hauptstadt";
+            break;
+        case "Schleswig-Holstein":
+            bundeslandInfoErgebnis.innerHTML = "Schleswig-Holstein hat 2,859 Mio Einwohner und Kiel ist die Hauptstadt";
+            break;
+        case "Thüringen":
+            bundeslandInfoErgebnis.innerHTML = "Thüringen hat 2,171 Mio Einwohner und Erfurt ist die Hauptstadt";
+            break;
+        default:
+            bundeslandInfoErgebnis.innerHTML = "Ein solches Bundesland gibt es in Deutschland nicht."
+
+    }
+
+}
+
+let myform = document.getElementById("myform");
+let mylist = document.getElementById("mylist");
+let masse = document.getElementById("masse");
+
+function showtxt() {
+    switch (mylist.value) {
+        case "0":
+            masse.innerHTML = "<b>Brief und Postkarte</b><br>L: 10 - 23,5 cm<br>B: 7 - 12,5 cm<br>H: bis 1 cm";
+            break;
+        case "1":
+            masse.innerHTML = "<b>DHL Paket 2 kg</b><br>bis 60 x 30 x 15 cm";
+            break;
+        case "2":
+            masse.innerHTML = "<b>DHL Paket 5 kg</b><br>bis 120 x 60 x 60 cm";
+            break;
+        case "3":
+            masse.innerHTML = "<b>DHL Paket 10 kg</b><br>bis 120 x 60 x 60 cm";
+            break;
+        default:
+            masse.innerHTML = "Wählen Sie ein Feld aus";
+    }
+}
