a5e1b15b27f2dea79210ee626a66f5d5a5f9bcf5
werthmannsupercode <werthmannsupercode@gmail.com>
2022-05-13 09:10:45 +0200

    add 2_3 & 2_4

---
+++ assets/js/lev2_3_2_4.js
@@ -0,0 +1,34 @@
+console.log("hi")
+
+// Lev2_3
+
+
+function diff(a) {
+    if (a > 27) {
+        console.log((a - 27) * 2);
+    }
+
+    else if (a <= 27) {
+        console.log(a - 27);
+    }
+}
+
+diff(35);
+diff(74);
+diff(123);
+
+// Lev2_4
+
+function sum(a, b) {
+    if (a == b) {
+        console.log((a + b) * 5);
+    }
+
+    else {
+        console.log(a + b);
+    }
+}
+
+sum(12, 54);
+sum(34, 34);
+sum(57, 9);
