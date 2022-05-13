df5f474b924db76e04aaa4d6249b1dc0d1b1419f
werthmannsupercode <werthmannsupercode@gmail.com>
2022-05-13 10:25:33 +0200

    Fehlerbehebung 2_3 &2_4

--- assets/js/lev2_3_2_4.js
+++ assets/js/lev2_3_2_4.js
@@ -5,30 +5,39 @@ console.log("hi")
 
 function diff(a) {
     if (a > 27) {
-        console.log((a - 27) * 2);
+        let case1 = (a - 27) * 2;
+        return case1;
     }
 
     else if (a <= 27) {
-        console.log(a - 27);
+        let case2 = a - 27;
+        return case2;
     }
 }
-
-diff(35);
-diff(74);
-diff(123);
+let result = diff(35);
+console.log(result);
+let result2 = diff(74);
+console.log(result2);
+let result3 = diff(140);
+console.log(result3);
 
 // Lev2_4
 
 function sum(a, b) {
     if (a == b) {
-        console.log((a + b) * 5);
+        let sum = (a + b) * 5;
+        return sum;
     }
 
     else {
-        console.log(a + b);
+        let sum = a + b;
+        return sum;
     }
 }
 
-sum(12, 54);
-sum(34, 34);
-sum(57, 9);
+let result4 = sum(12, 54);
+console.log(result4);
+let result5 = sum(34, 34);
+console.log(result5);
+let result6 = sum(57, 9);
+console.log(result6);
